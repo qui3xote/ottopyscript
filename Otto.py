51d2898a99520f7564cf3f447545f9fee7bb5a01
qui3xote <tmellis@gmail.com>
2021-12-27 16:01:57 -0500

    added pyscript interpreter class.

--- Otto.py
+++ Otto.py
@@ -10,6 +10,36 @@ DEBUG_AS_INFO = False
 
 registered_triggers = []
 
+class PyscriptInterpreter:
+    def set_state(self, fullname, value=None, attr_kwargs=None):
+        try:
+            state.set(fullname, value, **attr_kwargs)
+            return True
+        except:
+            log.warn(f"Unable to complete operation state.set({fullname}, {value}, **{attr_kwargs})")
+            return False
+
+    def get_state(self, fullname):
+        return state.get(fullname)
+
+    def service_call(self, domain, servicename, kwargs):
+        try:
+            service.call(domain,servicename,kwargs)
+            return True
+        except:
+            log.warn(f"Unable to complete operation state.set({fullname}, {value}, **{attr_kwargs})")
+            return False
+
+    def logger(self, msg, level):
+        log.info('called')
+        if level == 'info':
+            log.info(msg)
+        if level == 'debug':
+            if DEBUG_AS_INFO is True:
+                log.info(msg)
+        if level == 'warn':
+            log.warn(msg)
+
 
 class Otto:
     def __init__(self):
