5c001eab8e429d6ee47e78a424cec112f4567b02
qui3xote <tmellis@gmail.com>
2022-01-27 08:39:21 -0500

    worse bug fix...

--- interpreter.py
+++ interpreter.py
@@ -82,7 +82,7 @@ class PyscriptInterpreter:
                   entity_name,
                   value=None,
                   new_attributes=None,
-                  kwargs={}}):
+                  kwargs={}):
         try:
             self.log_debug(f"Setting {entity_name} to {value} with {kwargs}")
             state.set(entity_name, value, new_attributes, **kwargs)
